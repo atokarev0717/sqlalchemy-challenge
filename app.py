acab8d2bed2a8e7c6b99269c30f7c109094b251b
Andrey Tokarev <atokarev@Andreys-MacBook-Pro.local>
2021-01-26 00:58:15 -0700

    started working on flask app

--- app.py
+++ app.py
@@ -7,5 +7,15 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 
-
+# create engine to hawaii.sqlite
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
